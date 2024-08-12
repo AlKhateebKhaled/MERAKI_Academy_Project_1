5ac9afac3a707c71d41565521a301cd23062af11
AlKhateebKhaled <khaled.hkhateeb@gmail.com>
2024-08-12 23:40:03 +0300

    working on buttons logic

--- main.js
+++ main.js
@@ -12,17 +12,26 @@ main.append(questionDiv);
 
 const answerDiv = document.createElement("div");
 answerDiv.className = "answer";
-
+/*
 const maskedAnswerWord = document.createElement("h4");
 const word = "HANGMAN";
 const maskedWord = "The word is " + "# ".repeat(word.length);
 
 maskedAnswerWord.innerText = maskedWord;
 answerDiv.append(maskedAnswerWord);
+*/
+const word = "HANGMAN";
+let maskedWord = "#".repeat(word.length);
+
+const maskedAnswerWord = document.createElement("h3");
+maskedAnswerWord.innerText = maskedWord.split("").join(" ");
+answerDiv.append(maskedAnswerWord);
+
+main.append(answerDiv);
 
 const answerWord = document.createElement("h3");
-answerWord.textContent=[""];
-answerDiv.append(answerWord)
+answerWord.textContent = [""];
+answerDiv.append(answerWord);
 
 main.append(answerDiv);
 
@@ -38,6 +47,33 @@ chooseHeading.innerText = "Choose a character:";
 const buttonsDiv = document.createElement("div");
 buttonsDiv.className = "characters_buttons";
 
+/*const func_1=() => {
+   /* if (word.toLocaleUpperCase().includes (alphabetButton.textContent)){
+        answerWord.textContent.push(alphabetButton.textContent)
+    }
+   for(let i =0 ; i<word.length; i++)
+   if (word.includes (alphabetButton.textContent)){
+    maskedAnswerWord.innerText.split.splice(i, 1, alphabetButton.textContent)
+}
+    */
+const func_1 = (e) => {
+    const chosenChar = e.target.textContent; 
+    let newMaskedWord = maskedWord.split(""); 
+  
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] === chosenChar) {
+        newMaskedWord[i] = chosenChar; 
+      }
+    }
+  
+    maskedWord = newMaskedWord.join(""); 
+    maskedAnswerWord.innerText = maskedWord.split("").join(" "); 
+  
+    e.target.disabled = true; 
+    e.target.style.opacity = "0.6";
+    e.target.style.cursor = "not-allowed";
+};
+
 for (let i = 0; i < 26; i++) {
   const alphabetButton = document.createElement("button");
   alphabetButton.textContent = String.fromCharCode(65 + i); // A = 65, B = 66, ..., Z = 90
@@ -50,7 +86,7 @@ const imageDiv = document.createElement("div");
 imageDiv.className = "result";
 
 const imageResult = document.createElement("img");
-imageResult.src = "hangman.jpg";
+imageResult.src = "";
 imageResult.alt = "hangman result";
 imageDiv.append(imageResult);
 
@@ -58,8 +94,4 @@ choiceDiv.append(chooseHeading);
 choiceDiv.append(buttonsDiv);
 choiceDiv.append(imageDiv);
 
-const func_1=() => {
-    if (word.toLocaleUpperCase().includes (alphabetButton.textContent)){
-        answerWord.textContent.push(alphabetButton.textContent)
-    }
-}
+

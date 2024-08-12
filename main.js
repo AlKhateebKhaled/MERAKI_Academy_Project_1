24ef1ac6f28218e4b4ff3369f2af403c05c26347
AlKhateebKhaled <khaled.hkhateeb@gmail.com>
2024-08-12 17:10:57 +0300

    start DOM

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("11/8/2024")
+
+const buttonsDiv = document.createElement("div");
+buttonsDiv.className = "characters_buttons";
+
+for (let i = 0; i < 26; i++) {
+    const alphabetButton = document.createElement("button");
+    alphabetButton.textContent = String.fromCharCode(65 + i); // A = 65, B = 66, ..., Z = 90
+    buttonsDiv.append(alphabetButton);
+}
+
+const choiceDiv = document.querySelector(".choice");
+
+choiceDiv.appendChild(buttonsDiv);
